b199c594ab7a61593af7c9c166fb1831c320625f
gloreau <loreau.g@gmail.com>
2020-09-08 12:42:48 +0200

    Updates

---
+++ precache-manifest.ae854e6a7e033b92e0f7bcd13b0e8e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a3216786bef042781c10e402993265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9579762b92e3e9875dff",
+    "url": "/static/css/main.b7b7043f.chunk.css"
+  },
+  {
+    "revision": "cbb84cda916159b40cbc",
+    "url": "/static/js/2.3d007714.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d007714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9579762b92e3e9875dff",
+    "url": "/static/js/main.eabd866e.chunk.js"
+  },
+  {
+    "revision": "c4af187d4769a859b33d",
+    "url": "/static/js/runtime-main.4d467d91.js"
+  }
+]);
