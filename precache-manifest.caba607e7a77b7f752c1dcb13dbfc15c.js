c77adc7a4937cf469e5c044104715d24358403d3
gloreau <loreau.g@gmail.com>
2020-09-08 12:46:26 +0200

    Updates

--- precache-manifest.caba607e7a77b7f752c1dcb13dbfc15c.js
+++ precache-manifest.caba607e7a77b7f752c1dcb13dbfc15c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "36a3216786bef042781c10e402993265",
+    "revision": "c420c3b1e8027921ed8a2912726b1b98",
     "url": "/index.html"
   },
   {
-    "revision": "9579762b92e3e9875dff",
+    "revision": "5d5ce5ac9100d69b1cd5",
     "url": "/static/css/main.b7b7043f.chunk.css"
   },
   {
-    "revision": "cbb84cda916159b40cbc",
-    "url": "/static/js/2.3d007714.chunk.js"
+    "revision": "9327a4387fb5ac32727a",
+    "url": "/static/js/2.6743094d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.3d007714.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.6743094d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9579762b92e3e9875dff",
-    "url": "/static/js/main.eabd866e.chunk.js"
+    "revision": "5d5ce5ac9100d69b1cd5",
+    "url": "/static/js/main.5c43f1c6.chunk.js"
   },
   {
     "revision": "c4af187d4769a859b33d",
