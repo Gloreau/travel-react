d03d22b2935907911943d8a5586054debd9e405b
gloreau <loreau.g@gmail.com>
2020-09-08 13:42:51 +0200

    correction path

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 import Navbar from './components//Navbar';
 import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
-      <Router>
+      <HashRouter basename='/'>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
         </Switch>
-      </Router>
+      </HashRouter>
     </>
   );
 }
